35194a863ca1cd1a280ceaadd7ffeac4467566ae
Keegan Miller <Keegan.Miller@eilexinteractive.com>
2023-10-02 13:54:52 +1100

    Created the base tree class

--- Scripts/AI/BehaviorTree.cs
+++ Scripts/AI/BehaviorTree.cs
@@ -2,7 +2,22 @@ using System;
 using System.Collections.Generic;
 using Godot;
 
-public class BehaviorTree
+public abstract class BehaviorTree
 {
-    
+    protected Task mRootTask;
+    protected bool mIsInitialized = false;                      // If the tree has been set up
+
+    public void OnInitialize()
+    {
+        mRootTask = CreateTree();
+        mIsInitialized = true;
+    }
+
+    public void OnUpdate(float delta)
+    {
+        if(mIsInitialized)
+            mRootTask.RunTask();
+    }
+
+    protected abstract Task CreateTree();
 }
