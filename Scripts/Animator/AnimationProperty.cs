4125b2deb4097be23c83ad2fe62f78accd8d41b6
Keegan Miller <Keegan.Miller@eilexinteractive.com>
2023-10-02 11:43:03 +1100

    Created the animation property used for transitions

---
+++ Scripts/Animator/AnimationProperty.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using Godot;
+
+public enum EPropertyType
+{
+    INT = 0,
+    FLOAT = 1,
+    BOOL = 2
+}
+
+public abstract class AnimationProperty
+{
+    public string Key;
+    protected EPropertyType mType;                      // What type of property this is
+    public AnimationProperty(string key)
+    {
+        Key = key;
+    }
+}
+
+public class AnimationInt : AnimationProperty
+{
+    int Value;
+    AnimationInt(string key, int value) : base(key)
+    {
+        Value = value;
+        mType = EPropertyType.INT;
+    }
+}
+
+public class AnimationFloat : AnimationProperty
+{
+    float Value;
+    AnimationFloat(string key, float value) : base(key)
+    {
+        Value = value;
+        mType = EPropertyType.FLOAT;
+    }
+
+
+}
+
+public class AnimationBool : AnimationProperty
+{
+    bool Value;
+    AnimationBool(string key, bool value) : base(key)
+    {
+        Value = value;
+        mType = EPropertyType.BOOL;
+    }
+}
