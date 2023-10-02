cd70615337c3d886eec977ac9452337ab79fc9ef
Keegan Miller <Keegan.Miller@eilexinteractive.com>
2023-10-02 15:18:11 +1100

    Added AI movement

--- Scripts/Characters/BaseAI.cs
+++ Scripts/Characters/BaseAI.cs
@@ -5,6 +5,12 @@ using Godot;
 public partial class BaseAI : CharacterBody3D
 {
 
+    [ExportGroup("Movement Settings")]
+    [Export] protected NavigationAgent3D mAgent;
+    [Export] protected float mMovementSpeed;
+    protected Vector3 mTargetPosition;
+    public bool CanMove = false;
+
     // Reference to AI Components
     protected Blackboard mBlackboard;
     protected BehaviorTree mTree;
@@ -17,6 +23,33 @@ public partial class BaseAI : CharacterBody3D
         CreateBlackboard();
         mTree = new TestBT();
         mTree.OnInitialize(mBlackboard);
+    }
+
+    public void SetTargetPosition(Vector3 targetLocation)
+    {
+        mTargetPosition = targetLocation;
+        CanMove = true;
+    }
+
+    public void StopMovement()
+    {
+        CanMove = false;
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+        if(mAgent.IsNavigationFinished() || !CanMove)
+            return;
+
+        Vector3 currentAgentPosition = this.GlobalTransform.Origin;
+        Vector3 nextPathPoint = mAgent.GetNextPathPosition();
+
+        Vector3 createdVelocity = (nextPathPoint - currentAgentPosition).Normalized();
+        createdVelocity *= mMovementSpeed;
+
+        this.Velocity = createdVelocity;
+        MoveAndSlide();
     }
 
     /// <summary>
