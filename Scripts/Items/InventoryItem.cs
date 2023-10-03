8adb7bec5704965bb9959f5da44b97eba14bbab9
Keegan Miller <Keegan.Miller@eilexinteractive.com>
2023-10-03 12:58:46 +1100

    Added indicators

--- Scripts/Items/InventoryItem.cs
+++ Scripts/Items/InventoryItem.cs
@@ -7,10 +7,29 @@ public partial class InventoryItem : TextureButton
     [Export] private InventoryController mController;                       // Reference to the inventory controller
     [Export] private int mIndex;                            // Index of this button
 
-    public void Setup(InventoryStack item)
+    [ExportGroup("Indicators")]
+    [Export] private TextureRect mEquippedIndicator;
+    [Export] private TextureRect mCountIndicator;
+    [Export] private Label mStackCount;
+
+    public void Setup(InventoryStack item, bool equipped = false)
     {
         Item = item;
         this.TextureNormal = Item.Icon;
+
+        if(item.CurrentStackSize > 1)
+        {
+            mCountIndicator.Visible = true;
+            mStackCount.Text = item.CurrentStackSize.ToString();
+        }
+
+        if(equipped)
+        {
+            mEquippedIndicator.Visible = true;
+        } else 
+        {
+            mEquippedIndicator.Visible = false;
+        }
     }
 
     public void OnPressed()
