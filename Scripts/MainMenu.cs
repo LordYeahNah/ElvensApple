4de7d778e12cb5dbe575c9436f0d67e64521ac32
Keegan Miller <144700841+LordYeahNah@users.noreply.github.com>
2023-10-07 20:49:06 +1100

    Made inherit from canvas layer

--- Scripts/MainMenu.cs
+++ Scripts/MainMenu.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class MainMenu : Node
+public partial class MainMenu : CanvasLayer
 {
 	private Timer mSplashTimer;
 	private Timer mSplashFadeOutTimer;
