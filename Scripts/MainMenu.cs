9f8f659a8e5834b867ca1ac979c06944bbfef950
Keegan Miller <144700841+LordYeahNah@users.noreply.github.com>
2023-10-07 20:40:26 +1100

    Created the main menu to control the splash and main menu

--- Scripts/MainMenu.cs
+++ Scripts/MainMenu.cs
@@ -3,13 +3,33 @@ using System;
 
 public partial class MainMenu : Node
 {
-	// Called when the node enters the scene tree for the first time.
+	private Timer mSplashTimer;
+	private Timer mSplashFadeOutTimer;
+
+	[Export] private AnimationPlayer mSplashAnim;					// Reference to the animator for the splash room
+	[Export] private AnimationPlayer mMenuFadeIn;					// Reference to the animator for the main menu
+
+	[Export] private Control mSplashScreen;
+	[Export] private Control mMainMenu;
+	
 	public override void _Ready()
 	{
+		base._Ready();
+		mSplashTimer = new Timer(5.0f, false, StartSplashFadeOut, true);
+		mSplashFadeOutTimer = new Timer(1.0f, false, StartMenuFadeIn, false);
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+
+	private void StartSplashFadeOut()
 	{
+		mSplashAnim.Play("SplashFadeOut");
+		mSplashFadeOutTimer.IsActive = true;
+	}
+
+	private void StartMenuFadeIn()
+	{
+		mMenuFadeIn.Play("FadeIn");
+		mSplashScreen.Visible = false;
+		mMainMenu.Visible = false;
 	}
 }
