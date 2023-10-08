1eb00b5d8d77f7e2ea03b2c234379f84dd5b72ec
Keegan Miller <Keegan.Miller@eilexinteractive.com>
2023-10-08 21:55:05 +1100

    Added constructor

--- Scripts/Dialog/DialogData.cs
+++ Scripts/Dialog/DialogData.cs
@@ -6,11 +6,20 @@ public class DialogData
 {
     private string mDialogID;
     public string DialogID => mDialogID;
+    public string DialogName;
 
     private string mDialogMessage;
     public string DialogMessage => mDialogMessage;
 
     public event Action OnDialogComplete;
+
+    public DialogData(string id, string name, string message, Action dialogComplete)
+    {
+        OnDialogComplete += dialogComplete;
+        mDialogID = id;
+        DialogName = name;
+        mDialogMessage = message;
+    }
 
     public void CompleteDialog()
     {
