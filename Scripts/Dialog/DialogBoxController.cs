c09d7b3892ee1d4186a4183f9ccde53243646373
Keegan Miller <Keegan.Miller@eilexinteractive.com>
2023-10-08 21:45:03 +1100

    Create a controller for the dialog box

---
+++ Scripts/Dialog/DialogBoxController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Godot;
+
+public partial class DialogBoxController : Control
+{
+    private DialogData mCurrentDialog;
+
+    public void Setup(DialogData data)
+    {
+        mCurrentDialog = data;
+    }
+
+    public void OnComplete()
+    {
+        mCurrentDialog.CompleteDialog();
+    }
+}
