7ba6f370f4172a092aa73f890d4184b029794b7b
Keegan Miller <Keegan.Miller@eilexinteractive.com>
2023-10-08 21:57:36 +1100

    Added setting of the dialog text

--- Scripts/Dialog/DialogBoxController.cs
+++ Scripts/Dialog/DialogBoxController.cs
@@ -6,9 +6,16 @@ public partial class DialogBoxController : Control
 {
     private DialogData mCurrentDialog;
 
+    [Export] private Label mDialogName;                 // name of the character that you have interacted with
+    [Export] private Label mDialogMessage;
+    
+    
+
     public void Setup(DialogData data)
     {
         mCurrentDialog = data;
+        mDialogMessage.Text = mCurrentDialog.DialogMessage;
+        mDialogName.Text = mCurrentDialog.DialogName;
     }
 
     public void OnComplete()
