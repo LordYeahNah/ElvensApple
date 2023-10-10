0ddd20e5568edd00d672f7361e75dfe58af687b6
Keegan Miller <Keegan.Miller@eilexinteractive.com>
2023-10-10 11:09:18 +1100

    Added typing of the text

--- Scripts/Dialog/DialogBoxController.cs
+++ Scripts/Dialog/DialogBoxController.cs
@@ -8,14 +8,54 @@ public partial class DialogBoxController : Control
 
     [Export] private Label mDialogName;                 // name of the character that you have interacted with
     [Export] private Label mDialogMessage;
-    
-    
 
+    [ExportGroup("Timer Settings")] 
+    [Export] private float mPrintTime;                      // How frequently letters are typed
+    private Timer mPrintTimer;
+    private bool mIsTyping;
+
+    public bool IsTyping => mIsTyping;
+    
+    private int mMessageLength;
+    private int mTotalLetters = 0;
+    private string mCurrentMessage;                             // Reference to the dialog that has been printed
     public void Setup(DialogData data)
     {
         mCurrentDialog = data;
-        mDialogMessage.Text = mCurrentDialog.DialogMessage;
         mDialogName.Text = mCurrentDialog.DialogName;
+        mPrintTimer = new Timer(mPrintTime, true, OnUpdateLetter, true);
+        mIsTyping = true;
+        mMessageLength = mCurrentDialog.DialogMessage.Length;
+        mCurrentMessage = "";
+        mTotalLetters = 0;
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+        if(mPrintTimer != null)
+            mPrintTimer.OnUpdate((float)delta);
+    }
+
+    private void OnUpdateLetter()
+    {
+        mCurrentMessage += mCurrentDialog.DialogMessage[mTotalLetters];
+        mTotalLetters += 1;
+        if (mTotalLetters >= mMessageLength)
+        {
+            mPrintTimer.IsActive = false;
+            mIsTyping = false;
+        }
+
+        mDialogMessage.Text = mCurrentMessage;
+    }
+
+    public void CompleteMessage()
+    {
+        mCurrentMessage = mCurrentDialog.DialogMessage;
+        mIsTyping = false;
+        mPrintTimer.IsActive = false;
+        mDialogMessage.Text = mCurrentMessage;
     }
 
     public void OnComplete()
