35737a04dc4cccb9b280354532789d8961c8f7ed
Keegan Miller <Keegan.Miller@eilexinteractive.com>
2023-10-10 13:27:11 +1100

    Created a controller for the door to play animation and sound

---
+++ Scripts/DoorController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Godot;
+
+public partial class DoorController : Node3D
+{
+    [Export] private AudioStreamPlayer3D mAudio;
+    [Export] private AnimationPlayer mAnimation;
+
+    public void OpenDoor()
+    {
+        mAudio.Play();
+        mAnimation.Play();
+    }
+}
